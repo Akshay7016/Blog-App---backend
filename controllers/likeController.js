d7b795f27b690b877fff9334097a5d8cfe03f532
Akshay7016 <asajgule@gmail.com>
2023-04-27 20:21:52 +0530

    unlikePost api added

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -1,5 +1,6 @@
 const Post = require("../models/postModel");
 const Like = require("../models/likeModel");
+const { populate } = require("../models/postModel");
 
 exports.likePost = async (req, res) => {
     try {
@@ -23,6 +24,34 @@ exports.likePost = async (req, res) => {
             success: false,
             data: error.message,
             message: "Something went wrong"
+        })
+    }
+};
+
+exports.unlikePost = async (req, res) => {
+    try {
+        // which like want to delete based on like id
+        // from which post (id), particular like with like id wants to delete
+        const { post, like } = req.body;
+
+        // Delete like from Like model
+        await Like.findByIdAndDelete(like);
+
+        // Now delete entry from likes array of Post model
+        const updatedPost = await Post.findByIdAndUpdate(post, { $pull: { likes: like } }, { new: true })
+            .populate("likes")
+            .exec();
+
+        res.status(200).json({
+            success: true,
+            data: updatedPost,
+            message: "Unliked post"
+        })
+    } catch (error) {
+        res.status(503).json({
+            success: false,
+            data: error.message,
+            message: "Something went wrong!"
         })
     }
 };
